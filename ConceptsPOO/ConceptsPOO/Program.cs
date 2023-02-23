923234e2e5ea8a2427390493a21331559f10b19c
pablomonterof <pemfedu@gmail.com>
2023-02-23 10:23:43 -0500

    Employee & SalaryEmployee & CommissionEmployee Finish

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -5,13 +5,30 @@ using ConceptsPOO;
 Console.WriteLine("POO Concepts");
 Console.WriteLine("============");
 
-try
+Employee employee1 = new SalaryEmployee()
 {
-	Console.WriteLine(new Date(2024, 2, 29));
-	Console.WriteLine(new Date(1975, 3, 23));
-	Console.WriteLine(new Date(1985, 11, 30));
-}
-catch (Exception error)
+    Id = 1,
+    FirstName = "Sandra",
+    LastName = "Morales",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022, 1, 5),
+    IsActive = true,
+    Salary = 1815453.45M,
+};
+
+Console.WriteLine(employee1);
+
+Employee employee2 = new CommissionEmployee()
 {
-	Console.WriteLine(error.Message);
-}
+    Id = 2,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1995, 5, 23),
+    HiringDate = new Date(2022, 2, 5),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F
+    
+};
+
+Console.WriteLine(employee2);
